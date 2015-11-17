7496c8c0980fb014c32b1fbaa19f45b53eaf8dfd
akpil <jyk0802@naver.com>
2015-11-18 00:02:02 +0900

    Lab09 change

--- Lab09/calculator.js
+++ Lab09/calculator.js
@@ -2,7 +2,7 @@
 window.onload = function () {
     var stack = [];
 	var displayVal = "0";
-	var str = "";
+	var str = "0";
     for (var i in $$('button')) {
         $$('button')[i].onclick = function () {
             var value = this.innerHTML;
@@ -14,6 +14,12 @@ window.onload = function () {
 					displayVal = value;
 				}
 				else{
+					if(str.indexOf("=") != -1){
+						stack = [];
+						str="0";
+						document.getElementById("expression").innerHTML = "0";
+						displayVal = "";
+					}
 					if(displayVal.indexOf("!") == -1){
 						displayVal += value;
 					}
@@ -21,8 +27,10 @@ window.onload = function () {
 			}
 			else if(value == 'AC'){
 				displayVal = '0';
+				str = '0';
 				stack = [];
 				document.getElementById("expression").innerHTML = "0";
+				
 			}
 			else if(value == '.'){
 				if(displayVal.indexOf(".") == -1){
@@ -30,6 +38,11 @@ window.onload = function () {
 				}
 			}
 			else{
+				if(str.indexOf("=") != -1){
+					stack = [];
+					str="";
+					displayval = "0";
+				}
 				if(value == '!')
 				{
 					if(displayVal.indexOf("!") == -1){
@@ -37,11 +50,35 @@ window.onload = function () {
 					}
 				}
 				else{
-					stack.push(displayVal+value);
-				
-					str += displayVal+value;
+					if(str == '0'){
+						str = displayVal+value;
+						if(displayVal.indexOf("!") != -1)
+						{
+							var fact = factorial(parseFloat(displayVal));
+							stack.push(fact+value);
+						}
+						else{
+						stack.push(displayVal+value);
+						}
+					}
+					else{
+						str += displayVal+value;
+						if(displayVal.indexOf("!") != -1)
+						{
+							var fact = factorial(parseFloat(displayVal));
+							highPriorityCalculator(stack,fact+value);
+						}
+						else{
+							highPriorityCalculator(stack,displayVal+value);
+						}
+					}
+					if(str.indexOf("=") != -1){
+						displayVal = calculator(stack);
+					}
+					else{
+						displayVal='0';
+					}
 					document.getElementById("expression").innerHTML = str;
-					displayVal='0';
 					
 					
 				}
@@ -60,29 +97,37 @@ function factorial (x) {
 	}
 }
 function highPriorityCalculator(s, val) {
-	var lastval = s.pop;
+	var lastval = s.pop(s[s.length-1]);
 	var result = 0;
-	var operator = lastval.pop;
-	var valoperator = val.pop;
-	if(operator == "*"){
+	var operater = val[val.length-1];
+	if(lastval.indexOf("*") != -1){
 		result = parseFloat(lastval) * parseFloat(val);
-		s.push(result);
+		s.push(result+operater);
 	}
-	else if(operator == "^"){
-		result = parseFloat(lastval) ^ parseFloat(val);
-		s.push(result);
+	else if(lastval.indexOf("^") != -1){
+		result = Math.pow(parseFloat(lastval), parseFloat(val));
+		s.push(result+operater);
 	}
-	else if(operator == "/"){
+	else if(lastval.indexOf("/") != -1){
 		result = parseFloat(lastval) / parseFloat(val);
-		s.push(result);
+		s.push(result+operater);
 	}
-	else
+	else{
+		s.push(lastval);
+		s.push(val);
+	}
 }
 function calculator(s) {
     var result = 0;
     var operator = "+";
     for (var i=0; i< s.length; i++) {
-        
+		if(operator == "+"){
+        	result += parseFloat(s[i]);
+		}
+		else{
+			result -= parseFloat(s[i]);
+		}
+		operator = s[i][s[i].length-1];
     }
     return result;
 }
