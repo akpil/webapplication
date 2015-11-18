ec30ef4cf89de8cab0949ef52376153ab9f4041c
akpil <jyk0802@naver.com>
2015-11-18 21:45:48 +0900

    Lab10 finish

--- Lab10/game.js
+++ Lab10/game.js
@@ -19,6 +19,15 @@ function stopToStart(){
 
 function stopGame(){
 	$("state").innerHTML = "stop";
+	var blocks = $$(".block");
+	for(var i = 0; i < blocks.length; i++){
+		if(blocks[i].hasClassName("selected")){
+			blocks[i].removeClassName("selected");
+		}
+		if(blocks[i].hasClassName("target")){
+			blocks[i].removeClassName("target");
+		}
+	}
 	targetBlocks = [];
 	selectedBlocks = [];
 	timer=0;
@@ -75,12 +84,40 @@ function showToSelect(){
 	for(var i = 0; i < numberOfTarget; i++){
 		targetBlocks[i].removeClassName("target");
 	}
-	$("blocks").observe("click", event);
-	setTimeout(showToSelect, interval);
+	$("blocks").observe("click", function(event){
+		if(selectedBlocks.length >= numberOfTarget){}
+		else{
+			var checker = 0;
+			var selected = event.element();
+			for(var i = 0; i < selectedBlocks.length; i++){
+				if(selected == selectedBlocks[i]){
+					checker = 1;
+				}
+			}
+			if(checker == 0){
+				selectedBlocks.push(selected);
+				selected.addClassName("selected");
+			}
+		}
+	});
+	setTimeout(selectToResult, interval);
 }
 
 function selectToResult(){
-getScore;
-maxScore+=numberOfTarget;
-$("answer").innerHTML = getScore+"/"+maxScore;
+	$("state").innerHTML = "Checking";
+	maxScore+=numberOfTarget;
+	for(var i = 0; i < selectedBlocks.length; i++){
+		selectedBlocks[i].removeClassName("selected");
+		for(var j = 0; j < targetBlocks.length; j++){
+			if(selectedBlocks[i] == targetBlocks[j])
+			{
+				getScore += 1;
+			}
+		}
+	}
+	
+	$("answer").innerHTML = getScore+"/"+maxScore;
+	$("blocks").stopObserving();
+	
+	setTimeout(startToSetTarget,interval);
 }
